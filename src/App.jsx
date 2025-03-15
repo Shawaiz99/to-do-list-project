b7aec9e2d4b5d1039bca116a26cd97c6125334b1
Shawaiz <shawaizy2k@gmail.com>
2025-03-15 17:51:18 -0400

    Deleted nearly everything but the Navbar. Going to try it again

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import ThemeToggler from "./components/ThemeToggler";
+import ListForm from './components/ListForm';
 
 function App() {
+
+  const [lists, setList] = useState([]);
+
+  const addList = (newList) => {
+    console.log('New Task Added');
+    setList((prev) => [...prev, newList]);
+  };
+
+  const removeList = () => {
+    setList(null);
+  }
+
   return (
     <>
       <Navbar>
         <ThemeToggler/>
       </Navbar>
+      <div className='d-flex justify-content-center'>
+        <ListForm />
+      </div>
     </>
   );
 }
---
+++ src/components/ListForm.jsx
@@ -0,0 +1,9 @@
+function ListForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListForm
