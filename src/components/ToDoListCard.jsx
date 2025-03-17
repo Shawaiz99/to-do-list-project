8cc0f433bdf4985ec41ec0e52489622c7d9c66db
Shawaiz <shawaizy2k@gmail.com>
2025-03-17 15:23:04 -0400

    Added checkmark color to green

--- src/components/ToDoListCard.jsx
+++ src/components/ToDoListCard.jsx
@@ -6,7 +6,8 @@ function ToDoListCard() {
       <div className="card bg-bone-white text-dark">
         <div className="card-body d-flex justify-content-between">
             <div className="d-flex gap-1">
-              <label className="mt-2"><input type="radio" name="myRadio" value="option" /></label>
+              {/* TODO Change the checkbox to a radial design */}
+              <label className="mt-2"><input type="checkbox" name="myCheckbox" value="option" id="zug"/></label>
               <h3 className="card-title m-0">
                 Create a new task...
               </h3>
