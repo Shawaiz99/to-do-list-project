7967ea1a83cf212bff6d22c28954c2efaaf9cc3c
Shawaiz <shawaizy2k@gmail.com>
2025-03-17 13:27:54 -0400

    Added radial checkbox to card

--- src/components/ToDoListCard.jsx
+++ src/components/ToDoListCard.jsx
@@ -1,9 +1,19 @@
+import { FaTrash } from "react-icons/fa";
+
 function ToDoListCard() {
   return (
-    <div className="">
-      <div className="card">
-        <div className="card-body">
-            
+    <div className="col-6 m-5">
+      <div className="card bg-bone-white text-dark">
+        <div className="card-body d-flex justify-content-between">
+            <div className="d-flex">
+              <label><input type="radio" name="myRadio" value="option" /></label>
+              <h3 className="card-title m-0">
+                Create a new task...
+              </h3>
+            </div>
+            <div className="text-secondary">
+              <FaTrash />
+            </div>
         </div>
       </div>
     </div>
