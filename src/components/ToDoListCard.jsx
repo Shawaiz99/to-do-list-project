b48a2040bae6708f845d6be584b32244bb8fec25
Shawaiz <shawaizy2k@gmail.com>
2025-03-17 15:01:40 -0400

    Made some styling changes in order to center checkbox and text

--- src/components/ToDoListCard.jsx
+++ src/components/ToDoListCard.jsx
@@ -5,8 +5,8 @@ function ToDoListCard() {
     <div className="col-6 m-5">
       <div className="card bg-bone-white text-dark">
         <div className="card-body d-flex justify-content-between">
-            <div className="d-flex">
-              <label><input type="radio" name="myRadio" value="option" /></label>
+            <div className="d-flex gap-1">
+              <label className="mt-2"><input type="radio" name="myRadio" value="option" /></label>
               <h3 className="card-title m-0">
                 Create a new task...
               </h3>
